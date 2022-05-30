a682b78caa9ae17599a79019cff3c50a5489b01a
Lucian Nguyen <lucian.n@seznam.cz>
2022-05-30 21:52:30 +0200

    created login, register and watch pages. Will work on functionality

--- src/components/listItem/ListItem.jsx
+++ src/components/listItem/ListItem.jsx
@@ -1,6 +1,6 @@
 import "./listItem.scss";
 
-import React from "react";
+import React, { useState } from "react";
 import {
 	Add,
 	PlayArrow,
@@ -8,33 +8,45 @@ import {
 	ThumbUpAltOutlined,
 } from "@mui/icons-material";
 
-const ListItem = () => {
+const ListItem = ({ index }) => {
+	const [isHovered, setIsHovered] = useState(false);
+	const trailer =
+		"https://player.vimeo.com/external/371433846.sd.mp4?s=236da2f3c0fd273d2c6d9a064f3ae35579b2bbdf&profile_id=139&oauth2_token_id=57447761";
+
 	return (
-		<div className="listItem">
+		<div
+			className="listItem"
+			style={{ left: isHovered && index * 225 - 50 + index * 2.5 }}
+			onMouseEnter={() => setIsHovered(true)}
+			onMouseLeave={() => setIsHovered(false)}
+		>
 			<img
 				src="https://searchengineland.com/wp-content/seloads/2015/08/movie-film-video-production-ss-1920-800x450.jpg"
 				alt=""
 			/>
-			<div className="itemInfo">
-				<div className="icons">
-					<PlayArrow />
-					<Add />
-					<ThumbUpAltOutlined />
-					<ThumbDownAltOutlined />
-				</div>
-				<div className="itemInfoTop">
-					<span>1 hr 14 mins</span>
-					<span className="limit">+16</span>
-					<span>1996</span>
-				</div>
-				<div className="desc">
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora quasi
-					velit quas ab eius ratione, soluta, hic praesentium temporibus
-					adipisci voluptatum iste debitis ad sapiente dolorem natus repellendus
-					nostrum quae.
-				</div>
-				<div className="genre">Action</div>
-			</div>
+			{isHovered && (
+				<>
+					<video src={trailer} autoPlay={true} loop />
+					<div className="itemInfo">
+						<div className="icons">
+							<PlayArrow className="icon" />
+							<Add className="icon" />
+							<ThumbUpAltOutlined className="icon" />
+							<ThumbDownAltOutlined className="icon" />
+						</div>
+						<div className="itemInfoTop">
+							<span>1 hr 14 mins</span>
+							<span className="limit">+16</span>
+							<span>1996</span>
+						</div>
+						<div className="desc">
+							Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora
+							quasi velit quas ab eius ratione, soluta.
+						</div>
+						<div className="genre">Action</div>
+					</div>
+				</>
+			)}
 		</div>
 	);
 };
---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,35 @@
+import { useRef, useState } from "react";
+import "./login.scss";
+
+const Login = () => {
+	return (
+		<div className="login">
+			<div className="top">
+				<div className="wrapper">
+					<img
+						className="logo"
+						src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/799px-Netflix_2015_logo.svg.png"
+						alt="logo"
+					/>
+				</div>
+				<div className="container">
+					<form action="">
+						<h1>Sign In</h1>
+						<input type="email" placeholder="Enter email or phone number" />
+						<input type="password" placeholder="Enter password" />
+						<button className="loginButton">Sign in</button>
+						<span>
+							New to Netflix? <b>Sign up now.</b>
+						</span>
+						<small>
+							This page is protected by Google reCAPTCHA to ensure you're not a
+							bot. <b>Learn more</b> .
+						</small>
+					</form>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Login;
